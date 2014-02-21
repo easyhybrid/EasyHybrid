263c2d46a7befb957d1c0621acd3ca3d91a7211e
liuxuanzy <liuxuanzy@vip.qq.com>
2014-02-21 21:12:33 +0800

    fix package.json

--- tasks/hybrid.js
+++ tasks/hybrid.js
@@ -1,63 +1,244 @@
 var fs = require("fs");
 var path = require("path");
+var tpl = require("./lib/tpl");
+
 module.exports = function (grunt) {
 
-    grunt.task.registerTask("easy-hybrid-lib", "fetch each projects", function () {
-
-    });
-
-    grunt.task.registerTask("versions", function () {
-        console.log(grunt.config.data);
-    });
     grunt.task.loadNpmTasks("grunt-contrib-clean");
     grunt.task.loadNpmTasks("grunt-contrib-concat");
     grunt.task.loadNpmTasks("grunt-contrib-copy");
     grunt.task.loadNpmTasks("grunt-contrib-cssmin");
     grunt.task.loadNpmTasks("grunt-contrib-uglify");
+
+    grunt.task.registerTask("easy-hybrid-rename", "rename file", function () {
+        var mapping = grunt.file.expandMapping("**/js/**", ".tmp/compress/", {
+            cwd: ".tmp/source/",
+            rename: function (dest, src) {
+                if (src.indexOf(".") < 0) {
+                    return  src;
+                }
+                var arr = src.split(/\/|\\/);
+                var type = arr[0];
+                var basename = arr.pop() || "";
+                var match = new RegExp("^(?:(cordova-)?" + type + "-)(.+)$");
+                arr.push(basename.replace(match, "$1$2").replace(".tpl", ".js"));
+                return arr.join("/");
+            }
+        });
+        var match = /require *\( *['"](.+?)['"] *\)/ig;
+        mapping.forEach(function (item) {
+            var src = item.src[0];
+            var desc = item.dest;
+            if (desc.indexOf(".") < 0) {
+                grunt.file.mkdir(path.join(".tmp/compress", desc));
+                return;
+            }
+            var arr = desc.split(/\/|\\/);
+            var basename = arr.pop();
+            var type = arr[0];
+            grunt.file.copy(src, path.join(".tmp/compress", desc), {
+                encoding: "utf8",
+                process: function (content) {
+                    var p = arr.slice(2);
+                    var bp = basename.replace(type + "-", "").replace(".js", "");
+                    if (/\.tpl$/.test(src)) {
+                        bp += "-tpl";
+                    }
+                    p.push(bp);
+                    p.unshift("hybrid");
+                    if (/\.tpl$/.test(src)) {
+                        return 'define("' + p.join("/") + '", ' + tpl.run(content) + ');'
+                    } else {
+                        return 'define("' + p.join("/") + '", function (require, exports, module) {\n' + content.replace(match, function () {
+                            return "require(\"" + arguments[1].replace(type + "-", "").replace(".tpl", "-tpl") + "\")";
+                        }) + '\n});'
+                    }
+                }
+            });
+        });
+    });
+
     grunt.task.registerTask("easy-hybrid-rescue", "clean and rescue the old config", function () {
         grunt.config.init(grunt.config.get("_back"));
     });
 
+
     grunt.task.registerMultiTask("hybrid", "an javascript development approach base on cordova-js", function () {
-        var tasks = [];//任务列表
+        var me = this;
+        var tasks = ["copy:lib", "copy:source", "copy:platform", "clean:ora", "easy-hybrid-rename"];//任务列表
+        var lib_path = me.data.lib || "merge";
+        var pkg = me.data.pkg;
         var config = {
             _back: grunt.config.get(),//缓存现有文件
-            copy: {}//复制文件任务
-
-        };//配置文件
-        var lib_path = this.data.lib || "merge";
-        var flag = false;
-        if (lib_path === "package" || lib_path === "merge") {
-            config.copy.lib_package = {
-                cwd: 'node_modules/easy-hybrid/src/lib/',
+            clean: {
+                ora: [".tmp/ora"]
+            },
+            copy: {
+                lib: filterLirary(lib_path, pkg),//归并基础库
+                source: {//归并项目资源文件
+                    files: [
+                        {
+                            expand: true,
+                            cwd: 'src/' + me.target + "/",
+                            src: ["**", "!init.js", "!package.js", "!**/css/**"],
+                            dest: '.tmp/ora/js/view',
+                            filter: createFilter(pkg, false, "")
+                        },
+                        {
+                            expand: true,
+                            cwd: 'src/' + me.target + "/",
+                            src: ["init.js"],
+                            dest: '.tmp/ora/js/',
+                            filter: createFilter(pkg, false, "")
+                        }
+                    ]
+                },
+                platform: {//提取各平台文件
+                    files: [
+                        {
+                            expand: true,
+                            cwd: '.tmp/ora/',
+                            src: ["**"],
+                            dest: '.tmp/source/dev/',
+                            filter: createFilter(pkg, false, "web")
+                        }
+                    ]
+                }
+            }
+        };
+        //归并项目资源文件
+        var flist = fs.readdirSync(path.join(process.cwd(), 'src', me.target));
+        flist.forEach(function (item) {
+            config.copy.source.files.push({
+                expand: true,
+                cwd: 'src/' + me.target + "/" + item + "/css/",
                 src: ["**"],
-                dest: '.tmp/lib_ora_unfilter/'
-            };
-            flag = true;
-            tasks.push("copy:lib_package");
-        }
-        if (lib_path === "project" || lib_path === "merge") {
-            config.copy.lib_project = {
-                cwd: 'src/lib/',
+                dest: '.tmp/ora/css/',
+                filter: createFilter(pkg, false, "")
+            });
+        });
+        //提取各平台文件
+        pkg.platform.forEach(function (item) {
+            config.copy.platform.files.push({
+                expand: true,
+                cwd: '.tmp/ora/',
                 src: ["**"],
-                dest: '.tmp/lib_ora_unfilter/'
-            };
-            flag = true;
-            tasks.push("copy:lib_project");
-        }
-        if (!flag) {
-            config.copy.lib_user = {
-                cwd: lib_path,
-                src: ["**"],
-                dest: '.tmp/lib_ora_unfilter/'
-            };
-            tasks.push("copy:lib_user");
-        }
-        var pkg = this.data.pkg;
+                dest: '.tmp/source/' + item + "/",
+                filter: createFilter(pkg, false, item)
+            });
+        });
         grunt.config.init(config);
         tasks.push("easy-hybrid-rescue");
         grunt.task.run(tasks);
     });
 
+    /**
+     * 基础库复制条件生成
+     * @param lib_path 库路径类型
+     * @param pkg 程序配置包
+     * @returns {object} 过滤函数
+     */
+    function filterLirary(lib_path, pkg) {
+        var copy = {files: []};
+        var flag = false;
+        var filter = createFilter(pkg, true, "");
+        if (lib_path === "package" || lib_path === "merge") {
+            copy.files.push({
+                expand: true,
+                cwd: 'node_modules/easy-hybrid/src/lib/',
+                src: ["**", "!**/css/**"],
+                dest: '.tmp/ora/js/',
+                filter: filter
+            });
+            copy.files.push({
+                expand: true,
+                cwd: 'node_modules/easy-hybrid/src/lib/css/',
+                src: ["**"],
+                dest: '.tmp/ora/css',
+                filter: filter
+            });
+            flag = true;
+        }
+        if (lib_path === "project" || lib_path === "merge") {
+            copy.files.push({
+                expand: true,
+                cwd: 'src/lib/',
+                src: ["**", "!**/css/**"],
+                dest: '.tmp/ora/js/',
+                filter: filter
+            });
+            copy.files.push({
+                expand: true,
+                cwd: 'src/lib/css/',
+                src: ["**"],
+                dest: '.tmp/ora/css/',
+                filter: filter
+            });
+            flag = true;
+        }
+        if (!flag) {
+            copy.files.push({
+                expand: true,
+                cwd: lib_path,
+                src: ["**", "!**/css/**"],
+                dest: '.tmp/ora/js/',
+                filter: filter
+            });
+            copy.files.push({
+                expand: true,
+                cwd: lib_path + "css/",
+                src: ["**"],
+                dest: '.tmp/ora/css/',
+                filter: filter
+            });
+        }
+        return copy;
+    }
+
+    /**
+     * 用于生成复制过滤函数
+     * @param {object} pkg 程序配置包
+     * @param {bool} isLib 是不是库
+     * @param {string} platform 平台
+     * @returns {Function} 过滤函数
+     */
+    function createFilter(pkg, isLib, platform) {
+        var plats = "";
+        if (pkg.cordova) {//过滤掉所有的cordova开头的文件
+            plats += "cordova-|";
+        }
+        if (platform) {
+            pkg.platform.forEach(function (item) {
+                if (item === platform) {
+                    return;
+                }
+                plats += item + "-|";
+                plats += "cordova-" + item + "-|";
+            });
+        }
+        plats = plats.slice(0, -1);
+        var match = new RegExp("^(" + plats + ")");
+        if (isLib) {
+            return function (src) {
+                if (src.indexOf(".") < 0) {
+                    return true;
+                }
+                var arr = src.split(/\/|\\/);
+                var basename = (arr.pop() || "").split(".")[0] || "";
+                var type = arr.pop() || "";
+                return !(type in pkg) || pkg[type].include.indexOf(basename) >= 0 || !(pkg[type].exclude.indexOf(basename) >= 0 || plats && match.test(basename));
+            }
+        }
+        else {
+            return function (src) {
+                if (src.indexOf(".") < 0) {
+                    return true;
+                }
+                var arr = src.split(/\/|\\/);
+                var basename = (arr.pop() || "").split(".")[0] || "";
+                return !plats || !match.test(basename)
+            }
+        }
+    }
 };
 
---
+++ tasks/lib/tpl.js
@@ -0,0 +1,141 @@
+var fs = require('fs');
+
+var run = function (data) {
+    var reg = {
+        'if': function () {
+            return /{{\s*if\s*\(.+?}}/g;
+        },
+        'else': function () {
+            return /{{\s*else\s*}}/g;
+        },
+        'elseif': function () {
+            return /{{\s*else\s+if.+?}}/g;
+        },
+        'endif': function () {
+            return /{{\s*endif\s*}}/g;
+        },
+        'for': function () {
+            return /{{\s*for\s+.+?}}/g;
+        },
+        'endfor': function () {
+            return /{{\s*endfor\s*}}/g;
+        },
+        'eval': function () {
+            return /{{\s*eval\s+.+?}}/g;
+        }
+    };
+    var helper = {
+        variable: function (str) {
+            return str.replace(/{{(.+?)}}/, '$1');
+        },
+        statement: function (str, type) {
+            var result = '';
+            if (type == 'if') {
+                result = str.replace(/{{(.+?)}}/, '$1') + '{';
+            } else if (type == 'elseif' || type == 'else') {
+                result = '}' + str.replace(/{{(.+?)}}/, '$1') + '{';
+            } else if (type == 'for') {
+                str = str.replace(/^\s*\{\{/, '').replace(/\}\}\s*$/, '');
+                var list = str.match(/data=([\S]+)\s*/)[1];
+                var key = str.match(/key=([\S]+)\s*/)[1] || '_key';
+                var item = str.match(/item=([\S]+)\s*/)[1] || '_item';
+                var isObject = str.match(/object=([\S]+)\s*/); // object类型，使用for in
+                if (isObject && isObject[1]) {
+                    result = 'for(var ' + key + ' in ' + list + '){var ' + item + '=' + list + '[' + key + '];'
+                } else {
+                    var isReverse = str.match(/reverse=([\S]+)\s*/);
+                    var start = str.match(/start=([\S]+)\s*/);
+                    var end = str.match(/end=([\S]+)\s*/);
+                    var step = str.match(/step=([\S]+)\s*/);
+                    if (isReverse && isReverse[1]) {
+                        start = start ? start[1] : list + '.length - 1';
+                        end = end ? end[1] : -1;
+                        step = step ? -step[1] : -1;
+                        result = 'for(var ' + key + '=' + start + '; ' + key + '>' + end + ';' + key + '+=' + step + '){var ' + item + '=' + list + '[' + key + '];'
+                    } else {
+                        start = start ? start[1] : 0;
+                        end = end ? end[1] : list + '.length';
+                        step = step ? +step[1] : 1;
+                        result = 'for(var ' + key + '=' + start + '; ' + key + '<' + end + ';' + key + '+=' + step + '){var ' + item + '=' + list + '[' + key + '];'
+                    }
+                }
+            } else if (type == 'endif' || type == 'endfor') {
+                result = '}';
+            } else if (type == 'eval') {
+                result = str.replace(/^{{\s*eval/, '').replace(/}}\s*$/, '') + ';';
+            }
+            return result;
+        }
+    };
+    var preFlag = 1; // 上一次命中的类型：1：html文本，2：变量，3：语句
+    var flag; // 本次命中的类型
+    var source = ''; // 输出的源码
+    var isFirst = true; // 是否第一行
+    var str; // 循环中每行的代码
+    // 去注释空行，转义双引号和斜杆，根据语法进行分行，去空行，然后根据行切割
+    data = data.replace(/<!--[\w\W\r\n]*?-->/g, '').replace(/("|\\)/g, '\\$1').replace(/({{.+?}})/g,function (s) {
+        return '\n' + s + '\n';
+    }).replace(/\r/g, '');
+    data = data.split('\n').filter(function (v) {
+        return !/^\s*$/.test(v);
+    });
+
+    for (var i = 0, len = data.length; i < len; i++) {
+        str = data[i].replace(/^\s+|\s+$/, ' ');
+        if (str) {
+            var statementType = '';
+            if (/{{.+?}}/.test(str)) {
+                flag = 2;
+                for (var type in reg) {
+                    if (!reg.hasOwnProperty(type)) {
+                        continue;
+                    }
+                    if ((reg[type]()).test(str)) {
+                        flag = 3;
+                        statementType = type;
+                    }
+                }
+            } else {
+                flag = 1;
+            }
+            if (preFlag == 1) {
+                if (isFirst) {
+                    source += '\'';
+                }
+                if (flag == 1) {
+                    source += ' ' + str;
+                } else if (flag == 2) {
+                    source += '\',' + helper.variable(str);
+                } else if (flag == 3) {
+                    source += '\');' + helper.statement(str, statementType);
+                }
+            } else if (preFlag == 2) {
+                if (flag == 1) {
+                    source += ',\'' + str;
+                } else if (flag == 2) {
+                    source += ',' + helper.variable(str);
+                } else if (flag == 3) {
+                    source += ');' + helper.statement(str, statementType);
+                }
+            } else if (preFlag == 3) {
+                if (flag == 1) {
+                    source += '_s.push(\' ' + str;
+                } else if (flag == 2) {
+                    source += '_s.push(' + helper.variable(str);
+                } else if (flag == 3) {
+                    source += helper.statement(str, statementType);
+                }
+            }
+            isFirst = false;
+            preFlag = flag;
+        }
+    }
+    if (flag == 1) {
+        source += '\');';
+    } else if (flag == 2) {
+        source += ');';
+    }
+    return 'function(){return {render:function(map) {var p=[],v =[];for(var i in map) {p.push(i);v.push(map[i]);}return (new Function(p, \"var _s=[];_s.push(' + source + ' return _s;\")).apply(null, v).join("");}};}';
+
+};
+exports.run = run;
